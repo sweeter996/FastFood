8dda60701337632ae318d67b252405c12a53193b
Nguyen Phuong <sweeter996@gmail.com>
2017-11-10 00:57:32 +0700

    Login Form

---
+++ FastFood/DAO/HP_SetConnect_DAO.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+
+namespace DAO
+{
+    public class HP_SetConnect_DAO
+    {
+        string cnstr;
+        public SqlConnection cn;
+        public HP_SetConnect_DAO()
+        {
+            cnstr = System.Configuration.ConfigurationManager.ConnectionStrings["cnstr"].ConnectionString; ;
+            cn = new SqlConnection(cnstr);
+        }
+
+        public void Connect()
+        {
+            try
+            {
+                if (cn != null && cn.State == ConnectionState.Closed)
+                    cn.Open();
+            }
+            catch (SqlException ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public void DisConnect()
+        {
+            if (cn != null && cn.State == ConnectionState.Open)
+            {
+                cn.Close();
+            }
+        }
+
+        public SqlDataReader excuteNonquery(string query)
+        {
+            Connect();
+            SqlCommand cmd = new SqlCommand(query, cn);
+            return cmd.ExecuteReader();
+        }
+        public int excuteNonquery(string query, CommandType type, List<SqlParameter> paras)
+        {
+            try
+            {
+                Connect();
+                SqlCommand cmd = new SqlCommand(query, cn);
+                cmd.CommandType = type;
+                if (paras != null)
+                {
+                    foreach (SqlParameter para in paras)
+                    {
+                        cmd.Parameters.Add(para);
+                    }
+                }
+                cmd.ExecuteNonQuery();
+                return 1;
+            }
+            catch (SqlException ex)
+            {
+                return 0;
+                throw ex;
+            }
+        }
+
+        public DataTable GetSelectDAO(string select, string from)
+        {
+            try
+            {
+                string query = "Select " + select + "From " + from;
+                SqlDataAdapter adt = new SqlDataAdapter(query, cn);
+                DataTable dtSelect = new DataTable();
+                adt.Fill(dtSelect);
+                return dtSelect;
+            }
+            catch (SqlException ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                DisConnect();
+            }
+
+        }
+        public int Lookup(string keyName, string table)
+        {
+            Connect();
+            string query = "Select Count(" + keyName + ") From " + table;
+            SqlCommand command = new SqlCommand(query, cn);
+            int result = (int)command.ExecuteScalar();
+            DisConnect();
+            return result;
+        }
+
+    }
+}
---
+++ FastFood/FastFood/QL_NhanVien/UserLogin.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace FastFood.QL_NhanVien
+{
+    partial class UserLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbUserType = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btLogin = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Silver;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(119, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(322, 100);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đăng Nhập";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(196, 132);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(168, 26);
+            this.txtUsername.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tên Đăng Nhập";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(196, 164);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(168, 26);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(113, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Mật Khẩu";
+            // 
+            // cbUserType
+            // 
+            this.cbUserType.FormattingEnabled = true;
+            this.cbUserType.Items.AddRange(new object[] {
+            "Admin",
+            "User"});
+            this.cbUserType.Location = new System.Drawing.Point(196, 196);
+            this.cbUserType.Name = "cbUserType";
+            this.cbUserType.Size = new System.Drawing.Size(121, 28);
+            this.cbUserType.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(94, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Phân Quyền";
+            // 
+            // btLogin
+            // 
+            this.btLogin.Location = new System.Drawing.Point(196, 231);
+            this.btLogin.Name = "btLogin";
+            this.btLogin.Size = new System.Drawing.Size(121, 35);
+            this.btLogin.TabIndex = 5;
+            this.btLogin.Text = "Đăng Nhập";
+            this.btLogin.UseVisualStyleBackColor = true;
+            // 
+            // UserLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 293);
+            this.Controls.Add(this.btLogin);
+            this.Controls.Add(this.cbUserType);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "UserLogin";
+            this.Text = "UserLogin";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbUserType;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btLogin;
+    }
+}
